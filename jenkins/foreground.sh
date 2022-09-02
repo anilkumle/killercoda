a8fef05308f76a858c6168041285c15a5222e44a
Anil Kumle <anil.kumle@sakhaglobal.com>
2022-09-02 19:39:35 +0530

    added

--- jenkins/foreground.sh
+++ jenkins/foreground.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Installing Required dependencies..."
+                                   echo "Installing Required dependencies..."
 
 docker run -itd --name jenkins_server -p 8080:8080 -v jenkins_home:/var/jenkins_home \
      -v /var/run/docker.sock:/var/run/docker.sock \
        -v $(which docker):/usr/bin/docker sakhadevopsdocker/custom_jenknins:data
-       docker stop jenkins_server
+docker stop jenkins_server
 
 cd /var/lib/docker/volumes && chown -R ubuntu:ubuntu jenkins_home
 
@@ -13,10 +13,4 @@ docker restart jenkins_server
 
 
 
-
-
-
-
-
-
-echo "Jenkins server up and Running"
+                                      echo "Jenkins server up and Running"
